38870fcf623b690bf412b6ff38f0888edbe34b3d
Joshua Henderson <joshua.henderson@microchip.com>
2018-03-01 14:59:02 -0700

    docs: remove rst2pdf.pdfbuilder extension
    This is not completely necssary, and is by default a build failure on
    readthedocs.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'rst2pdf.pdfbuilder' ]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon' ]
 
 autoclass_content = 'both'
 autodoc_member_order = 'bysource'
